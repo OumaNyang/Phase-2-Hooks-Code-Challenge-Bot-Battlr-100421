ce8519b262b970978bf18ce92d1586707ee3f17a
Milton Ouma <ouma.nyang1@gmail.com>
2022-08-05 12:48:00 +0300

    First commit

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import YourBotArmy from "./YourBotArmy";
+import BotCollection from "./BotCollection";
 
-function YourBotArmy() {
-  //your bot army code here...
+
+const API = 'http://localhost:8002/bots';
+
+function BotsPage() {
+  //start here with your code for step one
+
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(res => res.json())
+      .then(setBots);
+  }, []);
+
+  function enlistBot(bot) {
+    setBots(bots.map((b) => (b.id === bot.id ? {...b, enlist: true } : b )));
+  }
+
+  function releaseBot(bot) {
+    setBots(bots.map((b) => (b.id === bot.id ? {...b, enlist: false } : b )));
+  }
+
+  function dischargeBot(bot) {
+    console.log('discharging', bot);
+    setBots(bots.filter((b) => b.id !== bot.id));
+  }
 
   return (
-    <div className="ui segment inverted olive bot-army">
-      <div className="ui five column grid">
-        <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
-        </div>
-      </div>
+    <div>
+      <YourBotArmy bots={bots.filter(b => b.enlist)} releaseBot={releaseBot} dischargeBot={dischargeBot}/>
+      <BotCollection bots={bots} enlistBot={enlistBot} dischargeBot={dischargeBot}/>
     </div>
-  );
+  )
 }
 
-export default YourBotArmy;
+export default BotsPage;
